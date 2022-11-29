8ae1c636448fb17d0fe717fa0526d95cea74de19
kennedybabu <kennedynjogu8@gmail.com>
2022-11-29 11:37:47 +0300

    style the sender and reciever messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className='flex mb-2'>
+    <div className='flex mb-2 gap-3 owner'>
       <div className='flex flex-col text-gray-400'>
         <div className='w-[50px] h-[50px] bg-red-400 rounded-full'>
           
@@ -10,7 +10,7 @@ const Message = () => {
         <span className='text-sm'>just now</span>
       </div>
       <div>
-        <p>hello</p>
+        <p className='bg-white px-[20px] py-[10px] rounded-tr-[10px] rounded-br-[10px] rounded-bl-[10px]'>hello</p>
       </div>
     </div>
   )
