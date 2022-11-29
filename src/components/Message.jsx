6ed728b96aa927d9e0c8cb85c76b3250b8b2b3ee
kennedybabu <kennedynjogu8@gmail.com>
2022-11-29 11:53:04 +0300

    feature: align sent and recieved messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className='flex mb-2 gap-3 owner'>
+    <div className='flex mb-2 gap-3 owner message'>
       <div className='flex flex-col text-gray-400'>
         <div className='w-[50px] h-[50px] bg-red-400 rounded-full'>
           
         </div>
         <span className='text-sm'>just now</span>
       </div>
-      <div>
-        <p className='bg-white px-[20px] py-[10px] rounded-tr-[10px] rounded-br-[10px] rounded-bl-[10px]'>hello</p>
+      <div className='content w-full'>
+        <p className='bg-white max-w-[max-content] px-[20px] py-[10px] rounded-tr-[10px] rounded-br-[10px] rounded-bl-[10px]'>hello</p>
+        <div className='w-[50%] h-[100px] bg-red-300 my-1'>
+
+        </div>
       </div>
     </div>
   )
