52a2cce8c263118f980a087534184cdc456ca222
kennedybabu <kennedynjogu8@gmail.com>
2022-11-29 09:35:41 +0300

    feature:create the messages component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import {BsFillCameraVideoFill} from "react-icons/bs"
+import {FaUserPlus} from "react-icons/fa"
+import {FiMoreHorizontal} from "react-icons/fi"
+
 
 const Chat = () => {
   return (
     <div className='chat'>
-      <div>
+      <div className='w-ful h-[60px] flex justify-between items-center px-4 bg-[#5d5b8d] rounded-tr-[10px]'>
         <span>kenny</span>
-        <div>
-          
+        <div className='flex h-full items-center text-gray-300'>
+            <BsFillCameraVideoFill className='mx-2 cursor-pointer'/>
+            <FaUserPlus className='mx-2 cursor-pointer'/>
+            <FiMoreHorizontal className='mx-2 cursor-pointer'/>
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center text-white justify-between bg-[#2f2d52] h-[60px] p-[10px] rounded-t-[10px]'>
+    <div className='flex items-center text-white justify-between bg-[#2f2d52] h-[60px] p-[10px] rounded-tl-[10px]'>
       <span className='font-bold'>chatty chat</span>
       <div className='flex items-center gap-[10px]'>
           <span>Kenny</span>
